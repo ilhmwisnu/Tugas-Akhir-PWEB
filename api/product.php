615ca277ec07fe992164ba418bacb5bc03e846b9
ilhmwisnu <ilhamwisnu42@gmail.com>
2022-06-16 15:11:56 +0700

    edit pagination

--- api/product.php
+++ api/product.php
@@ -5,12 +5,14 @@ require_once('../config/db.php');
 
 
 if(isset($_GET['sort'])){
+    // header("Content-type:application/json");
     echo json_encode([
         "data" => $product->get(),
         "pagination" => $pagination,
         "count" => $data->count_page()
     ]);
 }else{
+    // header("Content-type:application/json");
     echo json_encode([
         "data" => $product->get(),
     ]);
--- index.js
+++ index.js
@@ -1,78 +1,69 @@
 let currentPage = 1;
-let totalPage = 10;
+let totalPage = 8;
 
 let pagination = $("#pagination");
+
+$.get("./api/product.php", (data) => console.log(JSON.parse(data)["data"]));
+
+let pageBtn = (number, isActive) => {
+    return `<div id="page${number}" class="pageBtn px-4 py-2  ${
+        isActive ? "bg-black text-white " : "bg-light border border-dark"
+    }  rounded-2">${number}</div>`;
+};
+
+let insertBtn = (min, max, currentPage) => {
+    for (let index = min; index < max + 1; index++) {
+        pagination.html(
+            pagination.html() + pageBtn(index, index == currentPage)
+        );
+    }
+};
 
 function renderPagination(currentPage, totalPage) {
     pagination.html(
         `<div class=" first px-4 py-2 border border-dark rounded-2 bg-light">|<</div>`
     );
-    if (currentPage < 3) {
-        for (let index = 1; index < 1 + 5; index++) {
-            pagination.html(
-                pagination.html() +
-                    `<div id="page${index}" class="pageBtn px-4 py-2  ${
-                        index == currentPage
-                            ? "bg-black text-white "
-                            : "bg-light border border-dark"
-                    }  rounded-2">${index}</div>`
-            );
-        }
-    } else if (totalPage - currentPage < 3 ) {
-        for (let index = totalPage - 4; index < totalPage + 1; index++) {
-            pagination.html(
-                pagination.html() +
-                    `<div id="page${index}" class="pageBtn px-4 py-2  ${
-                        index == currentPage
-                            ? "bg-black text-white "
-                            : "bg-light border border-dark"
-                    }  rounded-2">${index}</div>`
-            );
-        }
-    }else {
-        for (let index = currentPage - 2; index < currentPage + 3; index++) {
-            pagination.html(
-                pagination.html() +
-                    `<div id="page${index}" class=" pageBtn px-4 py-2  ${
-                        index == currentPage
-                            ? "bg-black text-white "
-                            : "bg-light border border-dark"
-                    }  rounded-2">${index}</div>`
-            );
-        }
+    
+    if (totalPage <5) {
+        insertBtn(1,totalPage,currentPage)
+    }else if (currentPage < 3) {
+        insertBtn(1, 5, currentPage)
+    }else if (totalPage-currentPage < 3) {
+        insertBtn(totalPage-4, totalPage,currentPage)
+    }else{
+        insertBtn(currentPage-2, currentPage+2, currentPage)
     }
+
     pagination.html(
         pagination.html() +
             `<div class=" end px-4 py-2 border border-dark rounded-2 bg-light">>|</div>`
     );
 
-    $(".pageBtn").click((e)=>{
-        changePage(e)
-    })
-    $(".first").click((e)=>{
-        currentPage = 1
-        renderPagination(currentPage, totalPage)
-    })
-    $(".end").click((e)=>{
-        currentPage = totalPage
-        renderPagination(currentPage, totalPage)
-    })
+    $(".pageBtn").click((e) => {
+        changePage(e);
+    });
+    $(".first").click((e) => {
+        currentPage = 1;
+        renderPagination(currentPage, totalPage);
+    });
+    $(".end").click((e) => {
+        currentPage = totalPage;
+        renderPagination(currentPage, totalPage);
+    });
 }
 
-renderPagination(currentPage, totalPage)
-
-
-function changePage(e){
-    currentPage = parseInt(e.target.id.slice(4))
-    pagination.html("")
-    renderPagination(currentPage, totalPage)
+function changePage(e) {
+    currentPage = parseInt(e.target.id.slice(4));
+    pagination.html("");
+    renderPagination(currentPage, totalPage);
 }
 
-$("#sort").change(function (e) { 
-    console.log(e.target.value)
-    
+$("#sort").change(function (e) {
+    console.log(e.target.value);
 });
 
-$("#search").keyup(function (e) { 
-    console
+$("#search").keyup(function (e) {
+    console;
 });
+
+renderPagination(currentPage, totalPage);
